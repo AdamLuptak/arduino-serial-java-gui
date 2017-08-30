714a0a53afb19cc6e03f2ea96a680ea86624a0b8
root <root@root.com>
2017-08-30 08:12:15 +0200

    add toggle button , map table to pojo from arduino

--- java-arduino-fx-gui/src/main/java/Controller.java
+++ java-arduino-fx-gui/src/main/java/Controller.java
@@ -1,17 +1,32 @@
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.cell.PropertyValueFactory;
 import serialportcomunikator.ArduinoProxy;
 import serialportcomunikator.ArduinoResponseMessage;
 import serialportcomunikator.ArduinoSerialPortProxy;
 
 import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Observable;
 import java.util.Observer;
 
 
 public class Controller implements Observer {
 
+    @FXML
+    public ToggleButton startButton;
+    public TableColumn temp1Column;
+    public TableView temperatureTableView;
     ArduinoResponseMessage arduinoResponseMessage;
     Timer timer;
+    ObservableList<ArduinoResponseMessage> allData = FXCollections.observableArrayList();
 
     @FXML
     public void initialize() {
@@ -19,15 +34,28 @@ public class Controller implements Observer {
         arduinoProxy = new ArduinoSerialPortProxy();
         arduinoProxy.initialize();
         arduinoProxy.addObserver(this);
-        timer = new Timer(1000, t -> {
-            System.out.println("Updating values on front end");
-            System.out.println(arduinoResponseMessage);
-        });
-        timer.start();
+        temperatureTableView.setItems(allData);
+        timer = new Timer(1000, timerCallBack);
+        temp1Column.setCellValueFactory(new PropertyValueFactory<ArduinoResponseMessage,String>("temp1"));
+
     }
+
+    private ActionListener timerCallBack = t -> {
+        System.out.println("Updating values on front end");
+        System.out.println(arduinoResponseMessage);
+        allData.add(arduinoResponseMessage);
+    };
 
     private ArduinoSerialPortProxy arduinoProxy;
 
+    @FXML
+    public void startButtonToggle(ActionEvent event) {
+        if (startButton.isSelected()) {
+            timer.start();
+        } else {
+            timer.stop();
+        }
+    }
 
     @Override
     public void update(Observable o, Object arg) {
@@ -35,4 +63,5 @@ public class Controller implements Observer {
         ArduinoSerialPortProxy arduinoSerialPortProxy = (ArduinoSerialPortProxy) o;
         arduinoResponseMessage = arduinoSerialPortProxy.getData();
     }
+
 }
--- java-arduino-fx-gui/src/main/java/Main.java
+++ java-arduino-fx-gui/src/main/java/Main.java
@@ -1,8 +1,11 @@
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 import serialportcomunikator.ArduinoProxy;
 
 /**
@@ -14,9 +17,15 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("fxml/sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Arduino usb communicator");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setOnCloseRequest(t -> {
+            Platform.exit();
+            System.exit(0);
+        });
         primaryStage.show();
+
+
     }
 
 
